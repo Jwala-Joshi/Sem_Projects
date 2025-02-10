1b30799d7eb617e832881c248ba422b66e4c7335
Jwala Joshi <jwalajoshi71@gmail.com>
2025-02-10 14:54:34 +0545

    nav done

--- dashboard.cpp
+++ dashboard.cpp
@@ -4,9 +4,15 @@
 #include <QLabel>
 #include <QPushButton>
 #include <QSplitter>
+#include <QHBoxLayout>
 #include <QVBoxLayout>
 #include <QStackedWidget>
 #include <QWidget>
+#include <QSpacerItem>
+#include <QSizePolicy>
+#include <QMenu>
+#include <QAction>
+#include <QIcon>
 
 Dashboard::Dashboard(QWidget *parent)
     : QWidget(parent)
@@ -16,63 +22,137 @@ Dashboard::Dashboard(QWidget *parent)
     setWindowTitle("Dashboard");
     setMinimumSize(800,600);
 
-    QWidget *main =new QWidget;
+    QWidget *main = new QWidget;
     QVBoxLayout *lytMain = new QVBoxLayout(main);
 
     QSplitter *split = new QSplitter(Qt::Horizontal);
 
     QWidget *Menu = new QWidget;
-    QVBoxLayout *lytMenu =new QVBoxLayout(Menu);
+    Menu->setFixedWidth(200);
+    QVBoxLayout *lytMenu = new QVBoxLayout(Menu);
+    lytMenu->setSpacing(10);
+
+    QPushButton *btnMenu = new QPushButton;
+    QIcon menuIcon(":/resources/icons/hamburger-2.png");
+    btnMenu->setIcon(menuIcon);
+    btnMenu->setText("Menu");
+    btnMenu->setIconSize(QSize(20, 20));
+    btnMenu->setStyleSheet("QPushButton {background: transparent; border: none;}");
+    btnMenu->setFixedHeight(50);
 
     QPushButton *btnDashboard = new QPushButton("Dashboard");
     QPushButton *btnWorkoutPlans = new QPushButton("Workout Plans");
     QPushButton *btnChallenges = new QPushButton("Challenges");
     QPushButton *btnBuyProduct = new QPushButton("Buy Product");
     QPushButton *btnCalorieTracker = new QPushButton("Calorie Tracker");
-    QPushButton *btnSettings = new QPushButton("Settings");
 
+    QPushButton *btnSettings = new QPushButton;
+    QIcon settingsIcon(":/resources/icons/gear.png");
+    btnSettings->setIcon(settingsIcon);
+    btnSettings->setText("Settings");
+    btnSettings->setIconSize(QSize(20, 20));
+    btnSettings->setStyleSheet("QPushButton {background: transparent; border: none;}");
+    btnSettings->setFixedHeight(50);
+
+    QString buttonStyle = "QPushButton {"
+                          "background: transparent;"
+                          "border: none;"
+                          "font-size: 16px;"
+                          "color: #e6e6e6;"
+                          "padding: 8px 16px;"
+                          "text-align: left;}"
+                          "QPushButton:hover {"
+                          "color: #4d4d4d;"
+                          "background-color: #8c8c8c;}";
+
+    btnMenu->setStyleSheet(buttonStyle);
+    btnDashboard->setStyleSheet(buttonStyle);
+    btnWorkoutPlans->setStyleSheet(buttonStyle);
+    btnChallenges->setStyleSheet(buttonStyle);
+    btnBuyProduct->setStyleSheet(buttonStyle);
+    btnCalorieTracker->setStyleSheet(buttonStyle);
+    btnSettings->setStyleSheet(buttonStyle);
+
+    lytMenu->addWidget(btnMenu);
     lytMenu->addWidget(btnDashboard);
     lytMenu->addWidget(btnWorkoutPlans);
     lytMenu->addWidget(btnChallenges);
     lytMenu->addWidget(btnBuyProduct);
     lytMenu->addWidget(btnCalorieTracker);
+
+    QSpacerItem *spacer = new QSpacerItem(20, 40, QSizePolicy::Minimum, QSizePolicy::Expanding);
+    lytMenu->addItem(spacer);
+
     lytMenu->addWidget(btnSettings);
     Menu->setLayout(lytMenu);
 
     QStackedWidget *contentArea = new QStackedWidget;
 
-    // Create widgets for each content area
+    QWidget *header = new QWidget;
+    header->setFixedHeight(40);
+    QHBoxLayout *lytHeader = new QHBoxLayout(header);
+    lytHeader->setContentsMargins(10, 0, 0, 0);
+    lytHeader->setAlignment(Qt::AlignRight);
+
+    QLabel *lblWelcome = new QLabel("Welcome <Username>");
+    lblWelcome->setStyleSheet("font-size: 18px; color: #FFFFFF;");
+    lytHeader->addWidget(lblWelcome);
+
+    QPushButton *btnUserIcon = new QPushButton;
+    QIcon userIcon(":/resources/icons/user-dark.png");
+    btnUserIcon->setIcon(userIcon);
+    btnUserIcon->setIconSize(QSize(30, 30));
+    btnUserIcon->setStyleSheet("background: white; border: none;" "border-radius: 15px; padding:2px" );
+
+    QMenu *userMenu = new QMenu(this);
+    QAction *profile = new QAction("Profile", this);
+    QAction *upgrade = new QAction("Upgrade", this);
+    QAction *toggle =new QAction("Dark Mode",this);
+    QAction *setting =new QAction("Settings",this);
+    userMenu->addAction(profile);
+    userMenu->addAction(upgrade);
+    userMenu->addAction(toggle);
+    userMenu->addAction(setting);
+
+    connect(btnUserIcon, &QPushButton::clicked, this, [=]() {
+        userMenu->exec(QCursor::pos());
+    });
+
+    lytHeader->addWidget(btnUserIcon);
+
+    header->setLayout(lytHeader);
+
     QWidget *dashboardContent = new QWidget;
-    QLabel *lblDashboard = new QLabel("Welcome to the Dashboard!", dashboardContent);
     QVBoxLayout *dashboardLayout = new QVBoxLayout(dashboardContent);
+    dashboardLayout->addWidget(header);
+    QLabel *lblDashboard = new QLabel("Welcome to the Dashboard!", dashboardContent);
     dashboardLayout->addWidget(lblDashboard);
 
     QWidget *workoutPlansContent = new QWidget;
-    QLabel *lblWorkoutPlans = new QLabel("Here are your workout plans!", workoutPlansContent);
     QVBoxLayout *workoutPlansLayout = new QVBoxLayout(workoutPlansContent);
+    QLabel *lblWorkoutPlans = new QLabel("Here are your workout plans!", workoutPlansContent);
     workoutPlansLayout->addWidget(lblWorkoutPlans);
 
     QWidget *challengesContent = new QWidget;
-    QLabel *lblChallenges = new QLabel("Here are your challenges!", challengesContent);
     QVBoxLayout *challengesLayout = new QVBoxLayout(challengesContent);
+    QLabel *lblChallenges = new QLabel("Here are your challenges!", challengesContent);
     challengesLayout->addWidget(lblChallenges);
 
     QWidget *buyProductContent = new QWidget;
-    QLabel *lblBuyProduct = new QLabel("Buy products here!", buyProductContent);
     QVBoxLayout *buyProductLayout = new QVBoxLayout(buyProductContent);
+    QLabel *lblBuyProduct = new QLabel("Buy products here!", buyProductContent);
     buyProductLayout->addWidget(lblBuyProduct);
 
     QWidget *calorieTrackerContent = new QWidget;
-    QLabel *lblCalorieTracker = new QLabel("Track your calories here!", calorieTrackerContent);
     QVBoxLayout *calorieTrackerLayout = new QVBoxLayout(calorieTrackerContent);
+    QLabel *lblCalorieTracker = new QLabel("Track your calories here!", calorieTrackerContent);
     calorieTrackerLayout->addWidget(lblCalorieTracker);
 
     QWidget *settingsContent = new QWidget;
-    QLabel *lblSettings = new QLabel("Settings page.", settingsContent);
     QVBoxLayout *settingsLayout = new QVBoxLayout(settingsContent);
+    QLabel *lblSettings = new QLabel("Settings page.", settingsContent);
     settingsLayout->addWidget(lblSettings);
 
-    // Add all content widgets to QStackedWidget
     contentArea->addWidget(dashboardContent);
     contentArea->addWidget(workoutPlansContent);
     contentArea->addWidget(challengesContent);
@@ -80,32 +160,29 @@ Dashboard::Dashboard(QWidget *parent)
     contentArea->addWidget(calorieTrackerContent);
     contentArea->addWidget(settingsContent);
 
-    // Add widgets to the splitter (menu and content area)
     split->addWidget(Menu);
     split->addWidget(contentArea);
 
-    // Add the splitter to the main layout
     lytMain->addWidget(split);
     setLayout(lytMain);
 
-    // Connect buttons to switch between content
     connect(btnDashboard, &QPushButton::clicked, [=]() {
-        contentArea->setCurrentIndex(0);  // Switch to the Dashboard content
+        contentArea->setCurrentIndex(0);
     });
     connect(btnWorkoutPlans, &QPushButton::clicked, [=]() {
-        contentArea->setCurrentIndex(1);  // Switch to the Workout Plans content
+        contentArea->setCurrentIndex(1);
     });
     connect(btnChallenges, &QPushButton::clicked, [=]() {
-        contentArea->setCurrentIndex(2);  // Switch to the Challenges content
+        contentArea->setCurrentIndex(2);
     });
     connect(btnBuyProduct, &QPushButton::clicked, [=]() {
-        contentArea->setCurrentIndex(3);  // Switch to the Buy Product content
+        contentArea->setCurrentIndex(3);
     });
     connect(btnCalorieTracker, &QPushButton::clicked, [=]() {
-        contentArea->setCurrentIndex(4);  // Switch to the Calorie Tracker content
+        contentArea->setCurrentIndex(4);
     });
     connect(btnSettings, &QPushButton::clicked, [=]() {
-        contentArea->setCurrentIndex(5);  // Switch to the Settings content
+        contentArea->setCurrentIndex(5);
     });
 }
 
